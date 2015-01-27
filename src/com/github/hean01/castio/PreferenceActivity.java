6f69e6d1cf4bad9adabf39c1cd64249edbc2ee6a
Henrik Andersson <hean01@users.sourceforge.net>
2015-01-28 00:59:52 +0100

    Add actionbar title for prefs...

--- src/com/github/hean01/castio/PreferenceActivity.java
+++ src/com/github/hean01/castio/PreferenceActivity.java
@@ -1,5 +1,7 @@
 package com.github.hean01.castio;
+
 import android.os.Bundle;
+import android.app.ActionBar;
 
 public class PreferenceActivity extends android.preference.PreferenceActivity
 {
@@ -7,6 +9,10 @@ public class PreferenceActivity extends android.preference.PreferenceActivity
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+	ActionBar ab = getActionBar();
+	ab.setTitle("Preferences");
+	ab.setSubtitle("Setup service connection...");
+
         super.onCreate(savedInstanceState);
 	addPreferencesFromResource(R.xml.preferences);
     }
